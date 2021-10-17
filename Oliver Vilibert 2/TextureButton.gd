147ba896e65f00a46d307028beb00f68da7814f3
cosmiccat328 <74145515+cosmiccat328@users.noreply.github.com>
2021-10-17 14:54:50 +0300

    Godot 1.0

---
+++ Oliver Vilibert 2/TextureButton.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+var e = 1 #change code
+var a = 0 + e
+var b = "Game start"
+var d = "wasnt in game"
+var c = b + str(a) + d
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
